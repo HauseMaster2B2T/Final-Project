0267e7d315a0574738f03e080a05bf540956e835
HauseMaster2B2T <james.haydon@ocsbstudent.ca>
2021-03-05 01:07:52 +0000

    linter fixes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myMessage;
+let myMessage
 myMessage = 'INSTRUCTIONS: You need to enter your age and day of week (capitalizing first letter of weekday), then select the movie. Finally, last step is to click the buttons in this order: Age, Day, and Movie. Then the program will print the ticket price to the screen and give you the option to print the page in hard copy.'
 alert(myMessage)
 // Gets user input for age
@@ -11,14 +11,14 @@ function age () {
 document.getElementById('button2').addEventListener('click', day)
 // Stores user input in the variable day
 function day () {
-day = document.getElementById('input2').value
-//prints day to screen
-document.getElementById('result2').innerHTML = day
+  day = document.getElementById('input2').value
+// Prints day to screen
+  document.getElementById('result2').innerHTML = day
 }
-//loop to display available movies
-var movies = ['The Croods 2', 'Army of Darkness', 'Freaky', 'Stark Trek VI']
-var text = ''
-var i
+// Loop to display available movies
+let movies = ['The Croods 2', 'Army of Darkness', 'Freaky', 'Stark Trek VI']
+let text = ''
+let i
 for (i = 0; i < movies.length; i++) {
   text += movies[i] + '<br>'
 }
@@ -27,22 +27,22 @@ document.getElementById('myloop').innerHTML = text
 document.getElementById('button3').addEventListener('click', movie)
 // Stores user input in the variable movie
 function movie () {
-movie = document.getElementById('input3').value
-//prints movie to screen
-document.getElementById('result3').innerHTML = movie
-ticket() 
+  movie = document.getElementById('input3').value
+// Prints movie to screen
+  document.getElementById('result3').innerHTML = movie
+  ticket()
 }
-//compare age and day to each if else if statement 
-function ticket() {
-var price;
-     if (day == "Tuesday") { 
-     price = "Cheapie Tuesdays $5.00";
-   } else if (age >= 65) {
-     price = "Seniors $10.00";
-   } else if (age <= 12) {
-     price = "Juniors $8.00";
-   } else {
-     price = "General Admission $13.00"; 
-   }
-    document.getElementById("demo").innerHTML = price;
-}  
+// Compare age and day to each if else if statement
+function ticket () {
+  let price
+    if (day == 'Tuesday') {
+     price = 'Cheapie Tuesdays $5.00'
+  } else if (age >= 65) {
+     price = 'Seniors $10.00'
+  } else if (age <= 12) {
+     price = 'Juniors $8.00'
+  } else {
+     price = 'General Admission $13.00'
+  }
+    document.getElementById('demo').innerHTML = price
+}
