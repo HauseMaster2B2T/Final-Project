535e1c592983e66d3a907c8af39310c6ec9634ae
HauseMaster2B2T <james.haydon@ocsbstudent.ca>
2021-03-05 02:35:23 +0000

    script errors

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-myMessage = 'INSTRUCTIONS: You need to enter your age and day of week (capitalizing first letter of weekday), then select the movie. Finally, last step is to click the buttons in this order: Age, Day, and Movie. Then the program will print the ticket price to the screen and give you the option to print the page in hard copy.'
+const myMessage = 'INSTRUCTIONS: You need to enter your age and day of week (capitalizing first letter of weekday), then select the movie. Finally, last step is to click the buttons in this order: Age, Day, and Movie. Then the program will print the ticket price to the screen and give you the option to print the page in hard copy.'
 alert(myMessage)
 // Gets user input for age
 document.getElementById('button').addEventListener('click', age)
 // Stores user input in the variable age
 function age () {
-  age = document.getElementById('input').value
+  getage = document.getElementById('input').value
 }
 // Gets user input for day
 document.getElementById('button2').addEventListener('click', day)
 // Stores user input in the variable day
 function day () {
-  day = document.getElementById('input2').value
+  getday = document.getElementById('input2').value
   // Prints day to screen
-  document.getElementById('result2').innerHTML = day
+  document.getElementById('result2').innerHTML = getday
 }
 // Loop to display available movies
 const movies = ['The Croods 2', 'Army of Darkness', 'Freaky', 'Stark Trek VI']
@@ -26,19 +26,19 @@ document.getElementById('myloop').innerHTML = text
 document.getElementById('button3').addEventListener('click', movie)
 // Stores user input in the variable movie
 function movie () {
-  movie = document.getElementById('input3').value
+  getmovie = document.getElementById('input3').value
   // Prints movie to screen
-  document.getElementById('result3').innerHTML = movie
+  document.getElementById('result3').innerHTML = getmovie
   ticket()
 }
 // Compare age and day to each if else if statement
 function ticket () {
   let price
-  if (day === 'Tuesday' || day === 'tuesday' || day === 'tues' || day === 'Tues') {
+  if (getday === 'Tuesday' || getday === 'tuesday' || getday === 'tues' || getday === 'Tues') {
     price = 'Cheapie Tuesdays $5.00'
-  } else if (age >= 65) {
+  } else if (getage >= 65) {
     price = 'Seniors $10.00'
-  } else if (age <= 12) {
+  } else if (getage <= 12) {
     price = 'Juniors $8.00'
   } else {
     price = 'General Admission $13.00'
